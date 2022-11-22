ca48213b84ae20e0ddc662e8f3029a1e8c0040d4
Princepandey123 <74774500+princepandey123@users.noreply.github.com>
2022-11-23 00:08:32 +0530

    Update Assign5-DAA.cpp

--- Assign5-DAA.cpp
+++ Assign5-DAA.cpp
@@ -2,17 +2,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// binomialCoeff(n - 1,k-1)+ binomialCoeff(n-1,k)
+
 int binomialCoeff(int n, int k)
 {
 	
 	if (k > n)
 		return 0;
-	if (k == 0 || k == n)
-		return 1;
+	if ((n-k)<k)
+	     K=n-k;
 
-	
-	return binomialCoeff(n - 1, k - 1)
-		+ binomialCoeff(n - 1, k);
+        int dp[k+1];
+        memset(dp,0,sizeof(dp));
+         dp[0]=1;
+
+        for (int i=1;i<=n;i++)
+        { 
+           for( int j= min(k,i);j>0;j--)
+            {
+               dp[j] = (dp[j] + dp[j-1])%1000000007;
+             }
+        }
+
+	return dp[k];
+        
 }
 
 int main()
